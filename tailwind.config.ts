95218977f1424606e835b1d236ece97ead20a678
aaronfyr <aaronfoo76@hotmail.com>
2024-06-14 21:54:03 +0800

    End of section 2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
+// Import the Config type from the Tailwind CSS library
+import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
+// Define the Tailwind CSS configuration
 const config: Config = {
+  // Specify the paths to all of the template files in the project. Tailwind CSS will scan these files to generate the necessary styles.
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}", // All JavaScript, TypeScript, JSX, TSX, and MDX files in the pages directory
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}", // All JavaScript, TypeScript, JSX, TSX, and MDX files in the components directory
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // All JavaScript, TypeScript, JSX, TSX, and MDX files in the app directory
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}", // All JavaScript, TypeScript, JSX, and TSX files in the NextUI theme directory
   ],
+  // Extend the default theme
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  darkMode: 'class',
+  // Specify the plugins to be used (none in this case)
+  plugins: [nextui()],
 };
+
+// Export the configuration as the default export
 export default config;
