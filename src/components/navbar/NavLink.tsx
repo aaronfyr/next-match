17bbe1bdc2950f7b577d3cf041ef24fb5122fbc6
aaronfyr <aaronfoo76@hotmail.com>
2024-07-10 20:57:30 +0800

    End of section 8 updated v1

--- src/components/navbar/NavLink.tsx
+++ src/components/navbar/NavLink.tsx
@@ -1,20 +1,26 @@
 "use client";
 
+import useMessageStore from "@/hooks/useMessageStore";
 import { NavbarItem } from "@nextui-org/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
-type NavLinkProps = {
+type Props = {
 	href: string;
 	label: string;
 };
 
-export default function NavLink({ href, label }: NavLinkProps) {
+export default function NavLink({ href, label }: Props) {
 	const pathname = usePathname();
+	const { unreadCount } = useMessageStore((state) => ({
+		unreadCount: state.unreadCount,
+	}));
+
 	return (
 		<NavbarItem isActive={pathname === href} as={Link} href={href}>
-			{label}
+			<span>{label}</span>
+			{href === "/messages" && <span className="ml-1">({unreadCount})</span>}
 		</NavbarItem>
 	);
 }
